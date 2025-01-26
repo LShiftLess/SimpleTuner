ea33f422d6bdde2d780ebe4359065fee1cee5211
NoodleInWater <why097729@gmail.com>
2025-01-27 07:17:37 +0800

    Update datasets

--- datasets/generate_textfiles.py
+++ datasets/generate_textfiles.py
@@ -1,22 +1,24 @@
 import os
 from os import path
 
+from tqdm import tqdm
+
 
 root = path.dirname(__file__)
+cache_dir = path.join(path.split(root)[0], 'cache')
+cache_root = path.join(cache_dir, 'datasets')
 
 target_dirs = [
     'anime-girls',
     'anime-girls-r18',
 ]
 
-image_exts = [
-    '.png', '.jpg', '.jpeg'
-]
+image_exts = ['.png', '.jpg', '.jpeg']
 
-target_dirs = [path.join(root, dir) for dir in target_dirs]
+target_dirs = [path.join(cache_root, dir) for dir in target_dirs]
 
-for target_dir in target_dirs:
-    for file in os.listdir(target_dir):
+for target_dir in tqdm(target_dirs, colour='#ff0000'):
+    for file in tqdm(os.listdir(target_dir)):
         ext_splitted_file = path.splitext(file)
         file_name = ext_splitted_file[0]
         file_ext = ext_splitted_file[1]
--- datasets/resize_images.py
+++ datasets/resize_images.py
@@ -3,10 +3,13 @@ from os import path
 from PIL import Image
 from torchvision.transforms import transforms
 from PIL import ImageFile
+from tqdm import tqdm
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 root = path.dirname(__file__)
+cache_dir = path.join(path.split(root)[0], 'cache')
+cache_root = path.join(cache_dir, 'datasets')
 
 target_dirs = [
     "anime-girls",
@@ -17,19 +20,26 @@ image_exts = [".png", ".jpg", ".jpeg"]
 
 target_dirs = [path.join(root, dir) for dir in target_dirs]
 
-transformer = transforms.Compose([transforms.Resize((1024, 1024))])
+transformer = transforms.Compose([
+    transforms.Resize((1024, 1024))
+])
 
-for target_dir in target_dirs:
+os.makedirs(cache_dir, exist_ok=True)
+os.makedirs(cache_root, exist_ok=True)
 
-    dest_folder = f"{target_dir}-resized"
-    os.makedirs(dest_folder, exist_ok=True)
+for target_dir in tqdm(target_dirs, colour='#ff0000'):
 
-    for file in os.listdir(target_dir):
+    dataset_name = path.split(target_dir)[1]
+    dest_dir = path.join(cache_root, dataset_name)
+
+    os.makedirs(dest_dir, exist_ok=True)
+
+    for file in tqdm(os.listdir(target_dir)):
 
         file_ext = path.splitext(file)[1]
 
         if file_ext in image_exts:
-            dest_file = path.join(dest_folder, file)
+            dest_file = path.join(dest_dir, file)
             file = path.join(target_dir, file)
 
             img = Image.open(file)
@@ -37,5 +47,4 @@ for target_dir in target_dirs:
             img = transformer(img)
 
             img.save(dest_file, format="PNG")
-            print(f"Save resized image as {dest_file}")
             
